c6faf06a18102bffd2f42168af354a5db296b764
Kevin Lam <mtn013209@gmail.com>
2023-01-27 22:45:07 -0800

    starter

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Navbar, Body, Footer } from "../components"
+
+function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default HomePage
