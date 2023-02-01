a340adec4ad9c343968b03b4faa2e9328f8dce08
Kevin Lam <mtn013209@gmail.com>
2023-01-31 17:18:03 -0800

    b

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 import { HomePage } from "./pages/index.js"
 import "@picocss/pico"
-// ! pico imported here
+// ! pico imported here.
 
 function App() {
   return (
